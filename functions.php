f5b7c20dae91dc5677e82f7d0031d0f54ac72abd
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2023-03-07 15:13:47 -0500

    added new styles in build folder and font awesome

--- functions.php
+++ functions.php
@@ -11,7 +11,9 @@ add_action('wp_head', 'hook_css');
  */
 function alphatech_files()
 {
-    wp_enqueue_style('alphatech_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('alphatech_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('alphatech_extra_styles', get_theme_file_uri('/build/index.css'));
+    wp_enqueue_style('font-awesome', get_theme_file_uri('/assets/css/fontawesome.min.css'));
     //wp_enqueue_script('script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true);
 }
 
