3e13ed88af67c587920812e8d1f54363c022f41d
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2023-03-07 11:39:20 -0500

    added single and page files

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo "hi";
+
+
+
+while (have_posts()) {
+    the_post(); ?>
+
+    <h2><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
+    <p><?php the_content(); ?> </p>
+    <hr>
+<?php } ?>
---
+++ single.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package alpha-tech
+ *
+ 
+ */
+
+while (have_posts()) {
+    the_post(); ?>
+
+    <h2><?php echo the_title(); ?></h2>
+    <p><?php the_content(); ?> </p>
+    <hr>
+<?php } ?>
