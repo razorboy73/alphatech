2cbae118b69b8a24554b536b9673d431a84ec005
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2023-03-06 22:09:15 -0500

    fixing

--- index.php
+++ index.php
@@ -1,14 +1,2 @@
 <?php
-
-$names = array(
-    "Brad",
-    "Josh",
-    "Lili"
-);
-
-$count = 0;
-
-while ($count < count($names)) {
-    echo "<li>Hi my name is $names[$count]</li>";
-    $count++;
-}
+echo "hi";
