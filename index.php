60700beb8e4005216c15f025bd67ee3a3dafc973
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2023-03-06 22:03:32 -0500

    working on loops

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
-This is our custom theme.
-plese work out
+<?php
+
+$names = array(
+    "Brad",
+    "Josh",
+    "Lili"
+);
+
+$count = 0;
+
+while ($count < count($names)) {
+    echo "<li>Hi my name is $names[$count]</li>";
+    $count++;
+}
