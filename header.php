2ef7636b9c913b7628e1de9342c9d21d9084e32b
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2023-03-09 14:19:05 -0500

    added language atribute and body_class

--- header.php
+++ header.php
@@ -3,11 +3,11 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html>
+<html <?php language_attributes(); ?>>
 
 <head>
     <?php wp_head(); ?>
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title></title>
     <meta name="description" content="">
@@ -22,7 +22,7 @@
 </head>
 
 
-<body>
+<body <?php body_class(); ?>>
     <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
