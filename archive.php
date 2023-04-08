94030149ffb99326b3f3c1f195f2d635822674a3
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2023-04-08 12:14:10 -0400

    added single and archives for events

--- archive.php
+++ archive.php
@@ -13,10 +13,6 @@ get_header();
 
 
 
-
-
-
-get_header();
 ?>
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/alpha-college-library.jpg') ?>);"></div>
