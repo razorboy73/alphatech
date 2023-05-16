b8b2cfc7beccd08321a0dc1a2cd542c4d1494962
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2023-05-16 17:58:37 -0400

    43 page banner background

--- functions.php
+++ functions.php
@@ -31,6 +31,7 @@ function theme_infrastructure()
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'theme_infrastructure');
